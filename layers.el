3025bede9d0d139b6f57fbd748334b111926ebfd
G Derber <gd.github@drbr.org>
2026-04-09 11:30:31 -0400

    Not responsible for lost or stolen articles.

--- layers.el
+++ layers.el
@@ -66,7 +66,8 @@
 
 
    ;; 8. Fonts
-   unicode-fonts
+   (unicode-fonts :variables
+                  unicode-fints-enable-ligatures t)
 
    ;; 9. Fun (Conditionally loaded based on gui environment (See below))
    ;;emoji (Conditionally loaded based on gui environment)
@@ -174,9 +175,6 @@
      javascript
      ))
 
-<<<<<<< HEAD
-  )
-||||||| parent of 8aca10b (Release 0.1.0)
 (when (file-directory-p "~/Documents/Org")
   (configuration-layer/declare-layers
    '(
@@ -192,8 +190,6 @@
        org-enable-org-brain-support t
        org-enable-epub-support t)
      )))
-=======
-  )
 
 ;; Conditionally load ansible related layers
 (when (or
@@ -207,84 +203,6 @@
 ;; (when (file-directory-p org-directory)
 ;;   (load-file "~/.emacs.d/private/drbr/layers/org.el")
 ;;   )
->>>>>>> 8aca10b (Release 0.1.0)
-
-<<<<<<< HEAD
-;; Conditionally load ansible related layers
-(when (or
-       ;;(executable-find "ansible")
-       (file-directory-p "~/.ansible/collections/ansible_collections")
-       (file-directory-p "~/.ansible/inventories")
-       )
-  (load-file "~/.emacs.d/private/drbr/layers/ansible.el")
-  )
-
-;; (when (file-directory-p org-directory)
-;;   (load-file "~/.emacs.d/private/drbr/layers/org.el")
-;;   )
-||||||| parent of 8aca10b (Release 0.1.0)
-(when (file-directory-p "~/Documents/Development")
-  (configuration-layer/declare-layers
-   '(
-     ;; 15. Programming Languages
-     (c-c++ :variables
-            c-c++adopt-subprojects t
-            c-c++-enable-organize-includes-on-save t
-            c-c++backend 'lsp-clangd)
-     csv
-     graphviz
-     html
-     markdown
-     ;; lua
-     ;; php
-     (python :variables
-             python-backend 'anaconda
-             python-test-runner 'pytest
-             python-formatter 'yapf
-             python-format-on-save t
-             python-sort-imports-on-save t
-             python-fill-column 100
-             python-spacemacs-indent-guess nil)
-     ;; rust
-     shell-scripts
-     sql
-     ;; windows-scripts
-     yaml
-
-
-
-     ;; 22. Tools
-     cmake
-     debug
-     )))
-
-(when (executable-find "ansible")
-  (configuration-layer/declare-layers
-   '(
-     ansible
-     markdown
-     yaml
-
-)))
-=======
-(when (file-directory-p "~/Documents/Development")
-  (load-file "~/.emacs.d/private/drbr/layers/development.el")
-  )
->>>>>>> 8aca10b (Release 0.1.0)
-
-(when (or
-<<<<<<< HEAD
-       (file-directory-p "~/Src")
-||||||| parent of 8aca10b (Release 0.1.0)
-       (executable-find "ansible")
-=======
-       ;;(executable-find "ansible")
->>>>>>> 8aca10b (Release 0.1.0)
-       (file-directory-p "~/Documents/Development")
-<<<<<<< HEAD
-       )
-  (load-file "~/.emacs.d/private/drbr/layers/development.el")
-  )
 
 (when (or
        ;;(executable-find "ansible")
@@ -295,28 +213,6 @@
        (file-directory-p "~/.ansible/inventories")
        )
   (load-file "~/.emacs.d/private/drbr/layers/vcs.el")
-||||||| parent of 8aca10b (Release 0.1.0)
-       (file-directory-p "~/Documents/Org"))
-  (configuration-layer/declare-layers
-   ;; 18. Source Control
-  '(
-    (git :variables
-       git-enable-magit-gitflow-plugin t
-       git-enable-magit-delta-plugin t
-       git-enable-magit-todos-plugin t)
-    (version-control :variables
-                     version-control-diff-tool 'git-gutter
-                     version-control-diff-side 'left
-                     version-control-global-margin t)
-    )
-=======
-       (file-directory-p org-directory)
-       (file-directory-p "~/.ansible/collections/ansible_collections")
-       (file-directory-p "~/.ansible/inventories")
-       )
-  (load-file "~/.emacs.d/private/drbr/layers/vcs.el")
->>>>>>> 8aca10b (Release 0.1.0)
-  )
 
 (when (executable-find "apache2")
   (configuration-layer/declare-layers
